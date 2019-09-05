e488c9baa3e68e8273fe8c371055b831dcc2edf0
Reza <mrezzamoradi@gmail.com>
2019-09-06 01:05:54 +0430

    Update summary

--- app/js/resume.js
+++ app/js/resume.js
@@ -1,20 +1,36 @@
 const resume_python = {
     job_title: "Python Developer",
-    summary: "Respectful self-motivator gifted at finding reliable solution for software issues. Experienced in [Task] and [Task] and offering skills in [Skill] and [Skill]. ",
+    summary: "Passionate Python developer with 2+ years experience in web backend, API design, and async programming. " +
+        "With comprehensive knowledge about Network, DBs, Docker, Gitlab CI/CD, and monolithic and micro-services architectures, " +
+        "I am eager to expand my capabilities towards scaling, security and other DevOps concepts.",
     experience: [
+        {
+            className: "experience-title-devops",
+            title: "Junior DevOps Engineer",
+            company: "<a class='company' href='https://www.linkedin.com/company/fanavaran-etelaate-sokhan/'>Sokhan </a>, 2018 - Ongoing",
+            items: [
+                "Continuously collaborating with team members, including scrum masters to ensure a marked improvement in applications and the timely completion of projects.",
+                "Launched self-hosted tools like PYPI server, Phabricator, RocketChat, Gitlab-CE to facilitate production and communication needs of the company",
+                "Gathered and defined customer requirements to develop clear specifications for creating well-organized project plans.",
+                "Discussed project progress with customers, collected feedback on different stages and directly addressed concerns.",
+                "Evaluated potential new applications for existing products.",
+                "Collaborated with team members to improve the company’s engineering tools, systems and procedures.",
+                // "Sharing new experience and findings with the team members and helping them in tackling problems during the development of a project.",
+            ]
+        },
         {
             className: "experience-title-python",
             title: "Python Developer",
-            company: "<a class='company' href='https://www.linkedin.com/company/fanavaran-etelaate-sokhan/'>Sokhan </a>, 2017 - Ongoing",
+            company: "<a class='company' href='https://www.linkedin.com/company/fanavaran-etelaate-sokhan/'>Sokhan </a>, 2017 - 2018",
             items: [
                 "Supervising the development and deployment of back-end projects related to a virtual assistant platform which empowers our clients to provide their services using speech for Persian-speaking users.",
                 "Planning, writing, and reviewing many end-to-end and unit tests to improve security and performance of company products.",
-                "Working with both SQL and No-SQL databases.",
+                // "Working with both SQL and No-SQL databases.",
                 "Recognized several anti-patterns in code and applied fixes which reduced startup time of the virtual assistant <span class='highlight-bold'>from 20min down to 30s</span> and its <span class='highlight-bold'>memory usage by 60%</span> .",
                 "Developed a web dashboard to control and monitor the assistant. It boosted troubleshooting process by generating more comprehensible data.",
                 "Contributed to the implementation of a media server for audio and image files.",
                 "Implemented a fast API system based on an ASGI server, capable to process <span class='highlight-bold'>220 RPS</span> for a single user up to <span class='highlight-bold'>2000 RPS</span> for more than 10 users.",
-                "Built and deployed several web crawlers.",
+                // "Built and deployed several web crawlers.",
                 "Authored scripts to automate DB and storage backup mechanisms.",
                 "Documented and maintained wiki for some projects to be used by team developers and interns as they expand a project or develop new projects."
             ]
@@ -47,24 +63,38 @@ const resume_python = {
         {name: "CSS", level: 3},
         {name: "JS/JQuery", level: 2},
         {name: "Docker", level: 3},
+        {name: "Scrum", level: 2},
+        {name: "Virtualization", level: 1},
+        {name: "Network", level: 2},
     ],
-
     interests: [
-        "Skills related to DevOps",
+        "Automation and Scaling",
         "Node.js",
         "GraphQL",
-        "Machine Learning",
-        "Twisted",
+    ],
+    education: [
+        {
+            title: "Programming (Self Education)",
+            university: "",
+            items: [
+                "Completed several courses in <span class='highlight-unfocused'>Computer Science and Programming</span> from MIT OCW, edX and Coursera."
+            ]
+        },
+        {
+            title: "Master of Biotechnology & Bachelor of Engineering",
+            university: "Sharif University of Technology, Tehran, 2009-2015",
+        }
     ]
 };
-
 const resume_all = {
     job_title: "Entry Level DevOps Engineer",
-    summary: "Respectful self-motivator gifted at finding reliable solution for software issues. Experienced in [Task] and [Task] and offering skills in [Skill] and [Skill]. ",
+    summary: "Passionate Python developer with 2+ years of experience in web backend, API design, and async programming. " +
+        "With comprehensive knowledge about Network, DBs, Docker, Gitlab CI/CD, monolithic and microservices architectures, " +
+        "I am eager to expand my capabilities towards scaling, security and other DevOps concepts.",
     experience: [
         {
             className: "experience-title-devops",
-            title: "DevOps Engineer",
+            title: "Junior DevOps Engineer",
             company: "<a class='company' href='https://www.linkedin.com/company/fanavaran-etelaate-sokhan/'>Sokhan </a>, 2018 - Ongoing",
             items: [
                 "Continuously collaborating with team members, including scrum masters to ensure a marked improvement in applications and the timely completion of projects.",
@@ -83,7 +113,7 @@ const resume_all = {
             items: [
                 "Supervising the development and deployment of back-end projects related to a virtual assistant platform which empowers our clients to provide their services using speech for Persian-speaking users.",
                 "Planning, writing, and reviewing many end-to-end and unit tests to improve security and performance of company products.",
-                "Working with both SQL and No-SQL databases.",
+                // "Working with both SQL and No-SQL databases.",
                 "Recognized several anti-patterns in code and applied fixes which reduced startup time of the virtual assistant <span class='highlight-bold'>from 20min down to 30s</span> and its <span class='highlight-bold'>memory usage by 60%</span> .",
                 "Developed a web dashboard to control and monitor the assistant. It boosted troubleshooting process by generating more comprehensible data.",
                 "Contributed to the implementation of a media server for audio and image files.",
